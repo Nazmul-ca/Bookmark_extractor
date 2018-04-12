36c1d4ed63bf4a02bc3be5905ea48929b246de61
Nazmul Islam <nazmul.islam@dal.ca>
2018-04-11 22:09:04 -0300

    Add files via upload

---
+++ Extract_Bookmark/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Extract_book_mark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.show_book_mark = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // show_book_mark
+            // 
+            this.show_book_mark.Location = new System.Drawing.Point(467, 262);
+            this.show_book_mark.Name = "show_book_mark";
+            this.show_book_mark.Size = new System.Drawing.Size(201, 23);
+            this.show_book_mark.TabIndex = 8;
+            this.show_book_mark.Text = "Show book_mark";
+            this.show_book_mark.UseVisualStyleBackColor = true;
+            this.show_book_mark.Click += new System.EventHandler(this.show_book_mark_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(403, 470);
+            this.treeView1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(403, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(445, 27);
+            this.label1.TabIndex = 11;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(403, 447);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(445, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 470);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.show_book_mark);
+            this.Name = "Form1";
+            this.Text = "Extract bookmarks from PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button show_book_mark;
+    }
+}
+
---
+++ Extract_Bookmark/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+
+
+
+namespace Extract_book_mark
+{
+    public partial class Form1 : Form
+    {
+
+        TreeNode tn;
+        string title ;
+        PdfReader reader;
+        string file_name;
+        BackgroundWorker bgw;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;           
+        }
+
+        public void recursive_search(IList<Dictionary<string, object>> ilist, TreeNode tnt)
+        {
+            foreach (Dictionary<string, object> bk in ilist)
+            {
+                foreach (KeyValuePair<string, object> kvr in bk)
+                {
+                    if (kvr.Key == "Kids" || kvr.Key == "kids")
+                    {
+                        IList<Dictionary<string, object>> child = (IList<Dictionary<string, object>>)kvr.Value;
+                        recursive_search(child, tn);
+                    }
+                    else if (kvr.Key == "Title" || kvr.Key == "title")
+                    {
+                        tn = new System.Windows.Forms.TreeNode(kvr.Value.ToString());
+                        title = kvr.Value.ToString();                     
+                    }
+                    else if (kvr.Key == "Page" || kvr.Key == "page")
+                    {
+                        tn.ToolTipText = Regex.Match(kvr.Value.ToString(), "[0-9]+").Value;
+                        tnt.Nodes.Add(tn);
+                        ProgressChanged(title, kvr.Value.ToString());
+                    }
+                }
+            }
+        }
+        
+   
+
+        public void ProgressChanged(object sender, object value)
+        {
+            label1.Invoke((MethodInvoker)(() => label1.Text = "Extracting " + sender + "   page number " + value));
+        }
+
+        void bgw_DoWork(string reader_name)
+        {
+            reader = new iTextSharp.text.pdf.PdfReader(reader_name);
+           
+            IList<Dictionary<string, object>> book_mark = SimpleBookmark.GetBookmark(reader);
+
+            foreach (Dictionary<string, object> bk in book_mark)
+            {
+
+                foreach (KeyValuePair<string, object> kvr in bk)
+                {
+                    if (kvr.Key == "Kids" || kvr.Key == "kids")
+                    {
+                        IList<Dictionary<string, object>> child = (IList<Dictionary<string, object>>)kvr.Value;
+                        treeView1.Invoke((MethodInvoker)(() => recursive_search(child, tn)));
+                    }
+
+                    else if (kvr.Key == "Title" || kvr.Key == "title")
+                    {
+                        tn = new System.Windows.Forms.TreeNode(kvr.Value.ToString());
+                        title = kvr.Value.ToString();
+
+                    }
+                    else if (kvr.Key == "Page" || kvr.Key == "page")
+                    {
+                        //saves page number
+                        tn.ToolTipText = Regex.Match(kvr.Value.ToString(), "[0-9]+").Value;
+                        treeView1.Invoke((MethodInvoker)(() => treeView1.Nodes.Add(tn)));
+                        ProgressChanged(title, kvr.Value.ToString());
+                    }
+
+                }
+            }
+
+            label1.Invoke((MethodInvoker)(() => label1.Text = "book_mark Extraction completed"));
+            show_book_mark.Invoke((MethodInvoker)(() => show_book_mark.Enabled = true));
+            progressBar1.Invoke((MethodInvoker)(() => progressBar1.Visible = false));
+        }
+
+        private void show_book_mark_Click(object sender, EventArgs e)
+        {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    file_name = ofd.FileName;
+                }
+                else
+                    return;
+
+              label1.Text = "book_mark Extraction started";
+              show_book_mark.Enabled = false;
+              progressBar1.Visible = true;
+              bgw = new BackgroundWorker();
+
+              //backgroundworker initialization
+              bgw= new BackgroundWorker();
+              bgw.DoWork += ((obj, ep) => bgw_DoWork(file_name));
+              bgw.ProgressChanged += ((obj, ep) => ProgressChanged(obj, ep));
+              bgw.WorkerReportsProgress = true;
+              bgw.WorkerSupportsCancellation = true;
+              bgw.RunWorkerAsync();
+            
+        }
+ 
+    }
+}
